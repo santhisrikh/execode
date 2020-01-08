08d590c97013a96f90983eea753f2d481ba65287
vetrivel chinnasamy <26738977+vetrivelcsamy@users.noreply.github.com>
2020-01-08 17:37:34 +0530

    TodayContent Error Fixed

--- src/client/src/routes/Dashboard/User/Contest/TodayContest.jsx
+++ src/client/src/routes/Dashboard/User/Contest/TodayContest.jsx
@@ -2,8 +2,39 @@ import React from "react";
 
 const TodayContest = () => {
   return (
-    <div className="container">
-      <h1>TodayContest </h1>
+    <div>
+      <h4 className="lead ml-4">Today Contest</h4>
+      <hr />
+      <div className="container">
+        <div className="row">
+          <div className="col-12 col-md-6">
+            <div className="container  border  bg-light shadow">
+              <div className="row  mt-3 ">
+                <div className="col-8">
+                  <div className="row p-3">
+                    <h3 className="lead">Contest Name</h3>
+                  </div>
+                  <div className="row mb-2">
+                    <div className="col-4 border-right">Start Time</div>
+                    <div className="col-4 border-right">End Time</div>
+                    <div className="col-4 ">Date</div>
+                  </div>
+                </div>
+                <div className="col-4 p-3">
+                  <button
+                    type="submit"
+                    className="btn btn-success btn-block px-2"
+                  >
+                    Enter
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <hr />
+      <h4 className="lead ml-4">All Contests</h4>
     </div>
   );
 };
