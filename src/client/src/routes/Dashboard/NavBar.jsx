ed843b27d339e74ae12d2222def7bc75a9ed4dd0
albseb511 <albertsebastian511@gmail.com>
2020-01-18 15:08:48 +0530

    Added: Links for admin route in dashboard Navbar component

--- src/client/src/routes/Dashboard/NavBar.jsx
+++ src/client/src/routes/Dashboard/NavBar.jsx
@@ -1,10 +1,28 @@
 import React from "react";
 import { Link, NavLink } from "react-router-dom";
 
-const NavBar = () => {
+const NavBar = ({ location }) => {
+  const { pathname } = location;
+  let adminLinks = null;
+  if (pathname.startsWith("/dashboard/admin")) {
+    adminLinks = [
+      { name: "Contests", path: "all-contest" },
+      { name: "Create Contest", path: "create-contest" },
+      { name: "Contests", path: "create-challenge" }
+    ].map(link => (
+      <li role="presentation" className="nav-item">
+        <Link
+          className="nav-link text-light"
+          to={`/dashboard/admin/${link.path}`}
+        >
+          {link.name}
+        </Link>
+      </li>
+    ));
+  }
   return (
     <div>
-      <nav className="navbar navbar-dark bg-dark navbar-expand-md mb-4">
+      <nav className="navbar fixed navbar-dark bg-dark navbar-expand-md mb-4">
         <div className="container">
           <ul className="navbar-brand">
             <Link className="nav-link text-light" to="/dashboard">
@@ -22,9 +40,15 @@ const NavBar = () => {
           </div>
           <div className="collapse navbar-collapse" id="navcol-1">
             <ul className="nav navbar-nav ml-auto">
+              {adminLinks}
               <li role="presentation" className="nav-item">
                 <Link className="nav-link text-light" to="/dashboard">
                   Dashboard
+                </Link>
+              </li>
+              <li role="presentation" className="nav-item">
+                <Link className="nav-link text-light" to="/dashboard/admin">
+                  Admin
                 </Link>
               </li>
               <li role="presentation" className="nav-item">
